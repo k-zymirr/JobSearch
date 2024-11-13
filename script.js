4365d96da3ffeedf42840a24c21b39f31c0d1fc5
k_zymir <rphlstraub@gmail.com>
2024-11-13 20:23:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+    const squillRange = document.getElementById('noteSquill');
+    const squillNumber = document.getElementById('noteSquillNumber');
+
+    const locationRange = document.getElementById('noteLocation');
+    const locationNumber = document.getElementById('noteLocationNumber');
+
+    squillRange.addEventListener('input', function() {
+        squillNumber.value = squillRange.value;
+    });
+    squillNumber.addEventListener('input', function() {
+        let value = squillNumber.value;
+
+        if (value < 0){
+            value = 0;
+            squillNumber.value = 0;
+        }
+
+        if (value > 10){
+            value = 10;
+            squillNumber.value = 10;
+        }
+
+        if (value == ""){
+            squillRange.value = 0;
+        }else {
+            squillRange.value = value;
+        }
+    });
+
+    locationRange.addEventListener('input', function() {
+        locationNumber.value = locationRange.value;
+    });
+    locationNumber.addEventListener('input', function() {
+        let value = locationNumber.value;
+
+        if (value < 0){
+            value = 0;
+            locationNumber.value = 0;
+        }
+
+        if (value > 10){
+            value = 10;
+            locationNumber.value = 10;
+        }
+
+        if (value == ""){
+            locationRange.value = 0;
+        }else {
+            locationRange.value = value;
+        }
+    });
+}
