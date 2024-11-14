9aaaaf0d7c3dd351bdcfccb867ff9b0a13ae2a8b
k_zymir <k_zymir@BATLINUX>
2024-11-14 15:27:15 +0100

    ajout du traitement des cases 'postulé'

--- modify_offer.php
+++ modify_offer.php
@@ -12,6 +12,8 @@ if (!empty($_POST)) {
         $column = explode('-', htmlspecialchars($key))[0];
         if ($column == "postule"){
             $pdo->exec("UPDATE Offers SET applied = 1 WHERE id = " . explode('-', htmlspecialchars($key))[1]);
+        }elseif ($column == "reponse") {
+            $pdo->exec("UPDATE Offers SET response = '" . htmlspecialchars($value) . "' WHERE id = " . explode('-', htmlspecialchars($key))[1]);
         }
     }
 }
