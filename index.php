d8cf777cb3d72bbcd18ddb6d1b9eccbec2146e6e
k-zymirr <rphlstraub@gmail.com>
2024-11-19 06:35:58 +0100

    ajout version en + favicon

--- index.php
+++ index.php
@@ -1,119 +1,228 @@
 <?php require_once __DIR__ . "/config.php"?>
 
 <!DOCTYPE html>
-
-
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job Search</title>
+    <link rel="icon" href="img/favicon.png">
     <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 </head>
 <body>
-    <header>
-        <h1>Job Search</h1>
-    </header>
-    <button class="theme" onclick="changeTheme()"></button>
-    <div>
-        <h2>Ajouter une offre</h2>
-        <form action="add_offer.php" method="post">
-            <div>
-                <div>
-                    <input type="text" name="offer" id="offer" required>
-                    <label for="offer">Nom de l'offre</label>
-                </div>
-                <div>
-                    <input type="text" name="link" id="link" required>
-                    <label for="link">Lien de l'offre</label>
-                </div>
-            </div>
-            <div class="ranking">
+    <span class="fr">
+        <header>
+            <h1>Job Search</h1>
+        </header>
+        <button class="theme" onclick="changeTheme()"></button>
+        <button class="lang-fr" onclick="changeLanguage()"></button>
+        <div>
+            <h2>Ajouter une offre</h2>
+            <form action="add_offer.php" method="post">
                 <div>
                     <div>
-                        <input type="text" name="skill" id="skill" required>
-                        <label for="skill">Compétences demandées</label>
+                        <input type="text" name="offer" id="offer" required>
+                        <label for="offer">Nom de l'offre</label>
                     </div>
                     <div>
-                        <label for="noteSkill">Note</label>
-                        <input type="number" id="noteSkillNumber" name="noteSkillNumber" min="0" max="10" value="" required>
-                        <input type="range" id="noteSkill" name="noteSkill" min="0" max="10" value="0">
+                        <input type="text" name="link" id="link" required>
+                        <label for="link">Lien de l'offre</label>
                     </div>
                 </div>
-                <div>
+                <div class="ranking">
                     <div>
-                        <input type="text" name="location" id="location" required>
-                        <label for="location">Emplacement</label>
+                        <div>
+                            <input type="text" name="skill" id="skill" required>
+                            <label for="skill">Compétences demandées</label>
+                        </div>
+                        <div>
+                            <label for="noteSkill">Note</label>
+                            <input type="number" id="noteSkillNumber" name="noteSkillNumber" min="0" max="10" value="" required>
+                            <input type="range" id="noteSkill" name="noteSkill" min="0" max="10" value="0">
+                        </div>
                     </div>
                     <div>
-                        <label for="noteLocation">Note</label>
-                        <input type="number" id="noteLocationNumber" name="noteLocationNumber" min="0" max="10" value="" required>
-                        <input type="range" id="noteLocation" name="noteLocation" min="0" max="10" value="0">
+                        <div>
+                            <input type="text" name="location" id="location" required>
+                            <label for="location">Emplacement</label>
+                        </div>
+                        <div>
+                            <label for="noteLocation">Note</label>
+                            <input type="number" id="noteLocationNumber" name="noteLocationNumber" min="0" max="10" value="" required>
+                            <input type="range" id="noteLocation" name="noteLocation" min="0" max="10" value="0">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="send">
-                <input type="submit" value="Ajouter">
-                <input type="reset" value="Annuler">
-            </div>
-        </form>
-    </div>
+                <div class="send">
+                    <input type="submit" value="Ajouter">
+                    <input type="reset" value="Annuler">
+                </div>
+            </form>
+        </div>
 
-    <form action="modify_offer.php" method="post">
-        <?php
-            try {
-                $getOffers->execute();
-                $offers = $getOffers->fetchAll();
-
-                if (count($offers) > 0) {
-        ?>
-        <table>
-            <thead>
-                <tr>
-                    <td>Nom de l'offre</td>
-                    <td>Lien vers l'offre</td>
-                    <td>Compétences demandées</td>
-                    <td>Note</td>
-                    <td>Emplacement</td>
-                    <td>Note</td>
-                    <td>Score</td>
-                    <td>Postulé</td>
-                    <td>Réponse</td>
-                    <td>Supprimer</td>
-                </tr>
-            </thead>
-            <tbody>
+        <form action="modify_offer.php" method="post">
             <?php
-                foreach ($offers as &$offr):
+                try {
+                    $getOffers->execute();
+                    $offers = $getOffers->fetchAll();
+
+                    if (count($offers) > 0) {
             ?>
-                <tr>
-                    <td><?= $offr["name"] ?></td>
-                    <td class="center"><a href="<?= $offr["link"] ?>" target="_blank" rel="noopener noreferrer"><?= explode(".",$offr["link"])[1] ?></a></td>
-                    <td><?= $offr["skill"] ?></td>
-                    <td class="center"><?= $offr["noteSkill"] ?>/10</td>
-                    <td><?= $offr["location"] ?></td>
-                    <td class="center"><?= $offr["noteLocation"] ?>/10</td>
-                    <td class="center"><?= $offr["score"] ?>/20</td>
-                    <td class="center"><input type="checkbox" name="postule-<?= $offr["id"] ?>" id="postule-<?= $offr["id"] ?>" class="modifBase" <?= $offr["applied"] == 1 ? "checked" : "" ?>></td>
-                    <td><input type="text" name="reponse-<?= $offr["id"] ?>" id="reponse-<?= $offr["id"] ?>" class="modifBase" <?= strlen($offr["response"]) == 0 ? "" : "value='" . $offr["response"] . "'" ?>></td>
-                    <td>
-                        <label for="delete-<?= $offr["id"] ?>" class="del"><button type="button" onclick="toggleCheckbox(<?= $offr['id'] ?>)">Supprimer</button></label>
-                        <input type="checkbox" name="delete-<?= $offr["id"] ?>" id="delete-<?= $offr["id"] ?>" class="modifBase">
-                    </td>
-                </tr>
-        <?php
-                endforeach;
-        ?>
-            </tbody>
-        </table>
-        <input type="submit" value="submit" id="changed">
-        <?php
-        }
-            } catch (PDOException $e) {
-                //pass
+            <table>
+                <thead>
+                    <tr>
+                        <td>Nom de l'offre</td>
+                        <td>Lien vers l'offre</td>
+                        <td>Compétences demandées</td>
+                        <td>Note</td>
+                        <td>Emplacement</td>
+                        <td>Note</td>
+                        <td>Score</td>
+                        <td>Postulé</td>
+                        <td>Réponse</td>
+                        <td>Supprimer</td>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    foreach ($offers as &$offr):
+                ?>
+                    <tr>
+                        <td><?= $offr["name"] ?></td>
+                        <td class="center"><a href="<?= $offr["link"] ?>" target="_blank" rel="noopener noreferrer"><?= explode(".",$offr["link"])[1] ?></a></td>
+                        <td><?= $offr["skill"] ?></td>
+                        <td class="center"><?= $offr["noteSkill"] ?>/10</td>
+                        <td><?= $offr["location"] ?></td>
+                        <td class="center"><?= $offr["noteLocation"] ?>/10</td>
+                        <td class="center"><?= $offr["score"] ?>/20</td>
+                        <td class="center"><input type="checkbox" name="postule-<?= $offr["id"] ?>" id="postule-<?= $offr["id"] ?>" class="modifBase" <?= $offr["applied"] == 1 ? "checked" : "" ?>></td>
+                        <td><input type="text" name="reponse-<?= $offr["id"] ?>" id="reponse-<?= $offr["id"] ?>" class="modifBase" <?= strlen($offr["response"]) == 0 ? "" : "value='" . $offr["response"] . "'" ?>></td>
+                        <td>
+                            <label for="delete-<?= $offr["id"] ?>" class="del"><button type="button" onclick="toggleCheckbox(<?= $offr['id'] ?>)">Supprimer</button></label>
+                            <input type="checkbox" name="delete-<?= $offr["id"] ?>" id="delete-<?= $offr["id"] ?>" class="modifBase">
+                        </td>
+                    </tr>
+            <?php
+                    endforeach;
+            ?>
+                </tbody>
+            </table>
+            <input type="submit" value="submit" id="changed-fr">
+            <?php
             }
-        ?>
-    </form>
+                } catch (PDOException $e) {
+                    //pass
+                }
+            ?>
+        </form>
+    </span>
+
+    <span class="en" style="display:none;">
+        <header>
+            <h1>Job Search</h1>
+        </header>
+        <button class="theme" onclick="changeTheme()"></button>
+        <button class="lang-en" onclick="changeLanguage()"></button>
+        <div>
+            <h2>Add a Job Offer</h2>
+            <form action="add_offer.php" method="post">
+                <div>
+                    <div>
+                        <input type="text" name="offer" id="offer" required>
+                        <label for="offer">Job Offer Name</label>
+                    </div>
+                    <div>
+                        <input type="text" name="link" id="link" required>
+                        <label for="link">Job Offer Link</label>
+                    </div>
+                </div>
+                <div class="ranking">
+                    <div>
+                        <div>
+                            <input type="text" name="skill" id="skill" required>
+                            <label for="skill">Required Skills</label>
+                        </div>
+                        <div>
+                            <label for="noteSkill">Rating</label>
+                            <input type="number" id="noteSkillNumber" name="noteSkillNumber" min="0" max="10" value="" required>
+                            <input type="range" id="noteSkill" name="noteSkill" min="0" max="10" value="0">
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input type="text" name="location" id="location" required>
+                            <label for="location">Location</label>
+                        </div>
+                        <div>
+                            <label for="noteLocation">Rating</label>
+                            <input type="number" id="noteLocationNumber" name="noteLocationNumber" min="0" max="10" value="" required>
+                            <input type="range" id="noteLocation" name="noteLocation" min="0" max="10" value="0">
+                        </div>
+                    </div>
+                </div>
+                <div class="send">
+                    <input type="submit" value="Add">
+                    <input type="reset" value="Cancel">
+                </div>
+            </form>
+        </div>
+
+        <form action="modify_offer.php" method="post">
+            <?php
+                try {
+                    $getOffers->execute();
+                    $offers = $getOffers->fetchAll();
+
+                    if (count($offers) > 0) {
+            ?>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Job Offer Name</td>
+                        <td>Job Offer Link</td>
+                        <td>Required Skills</td>
+                        <td>Rating</td>
+                        <td>Location</td>
+                        <td>Rating</td>
+                        <td>Score</td>
+                        <td>Applied</td>
+                        <td>Response</td>
+                        <td>Delete</td>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    foreach ($offers as &$offr):
+                ?>
+                    <tr>
+                        <td><?= $offr["name"] ?></td>
+                        <td class="center"><a href="<?= $offr["link"] ?>" target="_blank" rel="noopener noreferrer"><?= explode(".",$offr["link"])[1] ?></a></td>
+                        <td><?= $offr["skill"] ?></td>
+                        <td class="center"><?= $offr["noteSkill"] ?>/10</td>
+                        <td><?= $offr["location"] ?></td>
+                        <td class="center"><?= $offr["noteLocation"] ?>/10</td>
+                        <td class="center"><?= $offr["score"] ?>/20</td>
+                        <td class="center"><input type="checkbox" name="postule-<?= $offr["id"] ?>" id="postule-<?= $offr["id"] ?>" class="modifBase" <?= $offr["applied"] == 1 ? "checked" : "" ?>></td>
+                        <td><input type="text" name="reponse-<?= $offr["id"] ?>" id="reponse-<?= $offr["id"] ?>" class="modifBase" <?= strlen($offr["response"]) == 0 ? "" : "value='" . $offr["response"] . "'" ?>></td>
+                        <td>
+                            <label for="delete-<?= $offr["id"] ?>" class="del"><button type="button" onclick="toggleCheckbox(<?= $offr['id'] ?>)">Delete</button></label>
+                            <input type="checkbox" name="delete-<?= $offr["id"] ?>" id="delete-<?= $offr["id"] ?>" class="modifBase">
+                        </td>
+                    </tr>
+            <?php
+                    endforeach;
+            ?>
+                </tbody>
+            </table>
+            <input type="submit" value="submit" id="changed-en">
+            <?php
+            }
+                } catch (PDOException $e) {
+                    //pass
+                }
+            ?>
+        </form>
+    </span>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,88 +1,136 @@
 window.onload = function() {
-    const skillRange = document.getElementById('noteSkill');
-    const skillNumber = document.getElementById('noteSkillNumber');
+    const skillRange = document.querySelectorAll('#noteSkill');
+    const skillNumber = document.querySelectorAll('#noteSkillNumber');
 
-    const locationRange = document.getElementById('noteLocation');
-    const locationNumber = document.getElementById('noteLocationNumber');
+    const locationRange = document.querySelectorAll('#noteLocation');
+    const locationNumber = document.querySelectorAll('#noteLocationNumber');
 
     const modifBase = document.getElementsByClassName('modifBase');
-
 
     if (localStorage.getItem('theme') == 'dark'){
         document.getElementsByTagName('body')[0].classList.add('darkTheme');
     }
 
-    skillRange.addEventListener('input', function() {
-        skillNumber.value = skillRange.value;
-    });
-    skillNumber.addEventListener('input', function() {
-        let value = skillNumber.value;
+    for (let i = 0; i < skillRange.length; i++) {
+        skillRange[i].addEventListener('input', function() {
+            skillNumber[i].value = skillRange[i].value;
+        });
+    }
 
-        if (value < 0){
-            value = 0;
-            skillNumber.value = 0;
-        }
+    for (let i = 0; i < skillNumber.length; i++) {
+        skillNumber[i].addEventListener('input', function() {
+            let value = skillNumber[i].value;
 
-        if (value > 10){
-            value = 10;
-            skillNumber.value = 10;
-        }
+            if (value < 0){
+                value = 0;
+                skillNumber[i].value = 0;
+            }
 
-        if (value == ""){
-            skillRange.value = 0;
-        }else {
-            skillRange.value = value;
-        }
-    });
+            if (value > 10){
+                value = 10;
+                skillNumber[i].value = 10;
+            }
 
-    locationRange.addEventListener('input', function() {
-        locationNumber.value = locationRange.value;
-    });
-    locationNumber.addEventListener('input', function() {
-        let value = locationNumber.value;
+            if (value == ""){
+                skillRange[i].value = 0;
+            } else {
+                skillRange[i].value = value;
+            }
+        });
+    }
 
-        if (value < 0){
-            value = 0;
-            locationNumber.value = 0;
-        }
+    for (let i = 0; i < locationRange.length; i++) {
+        locationRange[i].addEventListener('input', function() {
+            locationNumber[i].value = locationRange[i].value;
+        });
+    }
 
-        if (value > 10){
-            value = 10;
-            locationNumber.value = 10;
-        }
+    for (let i = 0; i < locationNumber.length; i++) {
+        locationNumber[i].addEventListener('input', function() {
+            let value = locationNumber[i].value;
 
-        if (value == ""){
-            locationRange.value = 0;
-        }else {
-            locationRange.value = value;
-        }
-    });
+            if (value < 0){
+                value = 0;
+                locationNumber[i].value = 0;
+            }
+
+            if (value > 10){
+                value = 10;
+                locationNumber[i].value = 10;
+            }
+
+            if (value == ""){
+                locationRange[i].value = 0;
+            } else {
+                locationRange[i].value = value;
+            }
+        });
+    }
 
     for (let i = 0; i < modifBase.length; i++) {
-        console.log(modifBase[i]);
         modifBase[i].addEventListener('change', function() {
-            document.getElementById('changed').click();
+            if (localStorage.getItem('lang') === 'fr') {
+                document.getElementById('changed-fr').click();
+            } else {
+                document.getElementById('changed-en').click();
+            }
         });
-    };
+    }
 
     document.querySelectorAll("div > input[type='text']").forEach(input => {
         input.parentNode.classList.add("inputGroup");
     });
-}
 
+    updateLanguage();
+}
 
 function changeTheme() {
     if (document.getElementsByClassName('darkTheme').length == 0){
         document.getElementsByTagName('body')[0].classList.add('darkTheme');
         localStorage.setItem('theme', 'dark');
-    }else {
+    } else {
         document.getElementsByTagName('body')[0].classList.remove('darkTheme');
         localStorage.setItem('theme', 'light');
     }
 }
 
+function changeLanguage() {
+    var currentLang = document.documentElement.lang;
+
+    if (currentLang === 'fr') {
+        document.documentElement.lang = 'en';
+    } else if (currentLang === 'en') {
+        document.documentElement.lang = 'fr';
+    }
+
+    localStorage.setItem('lang', document.documentElement.lang);
+    updateLanguage();
+    location.reload();
+}
+
+function updateLanguage() {
+    if (localStorage.getItem('lang') === 'fr') {
+        document.getElementsByClassName("fr")[0].style.display = "block";
+        document.getElementsByClassName("en")[0].style.display = "none";
+        document.documentElement.lang = 'fr';
+    } else if (localStorage.getItem('lang') === 'en') {
+        document.getElementsByClassName("fr")[0].style.display = "none";
+        document.getElementsByClassName("en")[0].style.display = "block";
+        document.documentElement.lang = 'en';
+    }
+}
+
 function toggleCheckbox(id) {
-    let checkbox = document.getElementById('delete-' + id);
+    let checkbox;
+    if (localStorage.getItem('lang') === 'fr') {
+        checkbox = document.querySelectorAll('#delete-' + id)[0];
+    }else {
+        checkbox = document.querySelectorAll('#delete-' + id)[1];
+    }
     checkbox.checked = !checkbox.checked;
-    document.getElementById('changed').click();
+    if (localStorage.getItem('lang') === 'fr') {
+        document.getElementById('changed-fr').click();
+    } else {
+        document.getElementById('changed-en').click();
+    }
 }
